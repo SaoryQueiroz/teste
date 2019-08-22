4599e1f87116355c1bda53ca2a4c21d79fb7ab03
Saory <saoryqueiroz17@gmail.com>
2019-08-22 09:09:38 -0400

    Primeiro commit

---
+++ src/testelp2git/TesteLp2Git.java
@@ -0,0 +1,16 @@
+
+package testelp2git;
+
+/**
+ *
+ * @author Saory
+ */
+public class TesteLp2Git {
+
+    public static void main(String[] args) {
+        //testando github
+        System.out.println("testelp2git");
+                
+    }
+    
+}
